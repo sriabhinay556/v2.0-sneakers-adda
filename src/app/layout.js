ebfcd624b770c5992658d33115b40ec107637fa5
Sriabhinay Kusuma <sriabhinayk@gmail.com>
2024-02-06 21:39:06 -0600

    homepage

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,40 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
+import logo from "./logo.jpg";
+import Image from 'next/image';
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "v2.0-sneakers-adda",
-  };
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <link href="https://fonts.googleapis.com/css2?family=BlackOpsOne:wght@400;700&display=swap" rel="stylesheet"/>
+      </head>
+      <body className={inter.className}>
+        <div className="flex flex-col sm:flex-row items-center justify-center space-x-0 sm:space-x-4 space-y-4 sm:space-y-0 p-4 border">
+          {/* Logo on the left */}
+          <div className="w-12 h-12 sm:w-16 sm:h-16 relative overflow-hidden rounded-full border">
+            <Image src={logo} alt="Logo" layout="fill" objectFit="cover" />
+          </div>
+
+          <h1 className="text-center text-3xl sm:text-5xl font-bold text-green-500 uppercase tracking-widest leading-loose" style={{fontFamily: "'BlackOpsOne', monospace"}}>
+            Sneakers Adda v2.0
+          </h1>
+
+          {/* Logo on the right */}
+          <div className="w-12 h-12 sm:w-16 sm:h-16 relative overflow-hidden rounded-full border">
+            <Image src={logo} alt="Logo" layout="fill" objectFit="cover" />
+          </div>
+        </div>
+
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,9 @@
-import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="p-10">
-      <p>v2.0 is still building..</p>
-      
-      <Link href="https://sneakers-adda.vercel.app" className="text-blue-500 hover:text-blue-700 underline cursor-pointer">
-      Check out my previous version! 
-      </Link>
+    <div>
+      /Home
     </div>
+
     );
 }
