1becf4bb263205718a1a313bdc8c2fce679b333a
Sriabhinay Kusuma <sriabhinayk@gmail.com>
2024-02-07 20:01:16 -0600

    commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,34 +13,32 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <link href="https://fonts.googleapis.com/css2?family=BlackOpsOne:wght@400;700&display=swap" rel="stylesheet"/>
-      </head>
+    
       <body className={inter.className}>
-        <div className="flex justify-center p-3">
+        {/* <div className="flex justify-center p-3">
           v2.0 is still developing-
           <Link href="https://sneakers-adda.vercel.app" className="text-blue-500 hover:text-blue-700 underline cursor-pointer">
           Check out my previous version!
           </Link>
 
-        </div>
-        <div className="flex flex-col sm:flex-row items-center justify-center space-x-0 sm:space-x-4 space-y-4 sm:space-y-0 p-4 border">
-          {/* Logo on the left */}
-          <div className="w-12 h-12 sm:w-16 sm:h-16 relative overflow-hidden rounded-full border">
-            <Image src={logo} alt="Logo" layout="fill" objectFit="cover" />
-          </div>
-
+        </div> */}
+        <div className="flex flex-col sm:flex-row items-center justify-center space-x-0 sm:space-x-4 space-y-4 sm:space-y-0 p-4 border mt-1">
+        
           <h1 className="text-center text-3xl sm:text-5xl font-bold text-green-500 uppercase tracking-widest leading-loose" style={{fontFamily: "'BlackOpsOne', monospace"}}>
             Sneakers Adda v2.0
           </h1>
 
-          {/* Logo on the right */}
-          <div className="w-12 h-12 sm:w-16 sm:h-16 relative overflow-hidden rounded-full border">
-            <Image src={logo} alt="Logo" layout="fill" objectFit="cover" />
-          </div>
         </div>
 
         {children}
+        <div className="flex flex-col sm:flex-row items-center justify-center space-x-0 sm:space-x-4 space-y-4 sm:space-y-0 p-4 border m-1">
+        
+          <h1 className="text-center text-3xl sm:text-5xl font-bold text-green-500 uppercase tracking-widest leading-loose" style={{fontFamily: "'BlackOpsOne', monospace"}}>
+           Footer
+          </h1>
+
+        </div>
+
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,18 @@
 
-export default function Home() {
-  return (
-    <div>
-      /Home
-    </div>
-
-    );
+async function getData() {
+  const res = await fetch('https://65c43452dae2304e92e25de9.mockapi.io/TestAPI/v1/users')
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+ 
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+ 
+  return res.json()
+}
+ 
+export default async function Home() {
+  const data = await getData()
+  return <main>{JSON.stringify(data)}</main>
 }
