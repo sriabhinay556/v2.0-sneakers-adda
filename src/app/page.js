d3455fdf589d7001414595253dfbe8dca26e05d7
Sriabhinay Kusuma <sriabhinayk@gmail.com>
2024-02-07 23:03:39 -0600

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,19 @@
-async function getData() {
-  const res = await fetch('https://65c43452dae2304e92e25de9.mockapi.io/TestAPI/v1/users', { next: { revalidate: 10 } })
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
- 
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data')
-  }
- 
-  return res.json()
-}
- 
-export default async function Home() {
-  const data = await getData()
-  //return <main>{JSON.stringify(data)}</main>
-  return  <div className="flex justify-center items-center p-5">
-  <div className="border border-gray-200 w-full max-w-[1000px] h-[530px] md:w-[1000px] md:h-[530px] lg:h-[620px]">
-    /Home
-  </div>
-</div>
+export default function Home() {
+  const items = ["AJ 1 Lows", "Yeezy 350's", "Air Jordan 3's", "Yeezy 500's", "AJ 1 Lows", "Yeezy 350's", "Air Jordan 3's", "Yeezy 500's", "AJ 1 Lows", "Yeezy 350's"];
+  return (
+    <div className="flex justify-center items-center p-5">
+      <div className="border border-gray-200 w-full max-w-[1000px] h-[530px] md:w-[1000px] md:h-[530px] lg:h-[620px] overflow-auto">
+        <div className="flex flex-col items-center w-full">
+          {
+            items.map((item, index) => (
+              // Adjust button width: full width on smaller screens, 1/4 width on larger screens
+              <button key={index} className="bg-blue-500 text-white w-full md:w-1/4 py-2 my-2 hover:bg-blue-700 transition-colors duration-150 ease-in-out">
+                {item}
+              </button>
+            ))
+          }
+        </div>
+      </div>
+    </div>
+  );
 }
