320deeb7ae9853873fd9e9622a41e56ae29b3c1d
Sriabhinay Kusuma <sriabhinayk@gmail.com>
2024-02-07 20:04:41 -0600

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 
 async function getData() {
-  const res = await fetch('https://65c43452dae2304e92e25de9.mockapi.io/TestAPI/v1/users')
+  const res = await fetch('https://65c43452dae2304e92e25de9.mockapi.io/TestAPI/v1/users', { next: { revalidate: 10 } })
   // The return value is *not* serialized
   // You can return Date, Map, Set, etc.
  
