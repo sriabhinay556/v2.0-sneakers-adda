cd3ca4abcac564b5a39411e6cd4f573c4a2983ce
Sriabhinay Kusuma <sriabhinayk@gmail.com>
2024-02-13 12:10:15 -0600

    commit

--- src/app/client_components/CarouselApp.js
+++ src/app/client_components/CarouselApp.js
@@ -19,8 +19,8 @@ function CarouselApp({ images }) {
           centerSlidePercentage={30}
         >
           {images.map((item) => (
-            <Link href={`/sneakers/${item.route}`}>
-            <div key={item.alt} className="flex flex-col items-center justify-center">
+            <Link key={item.alt} href={`/sneakers/${item.route}`}>
+            <div className="flex flex-col items-center justify-center">
               <Image src={item.src} alt={item.alt} width={500} height={300} layout="responsive" />
               <p className="text-center font-bold py-10">{item.alt}</p>
             </div>
@@ -33,8 +33,8 @@ function CarouselApp({ images }) {
               <div className='md:hidden' style={{ maxHeight: '90vh' }}>
           <div className="flex flex-col items-center justify-center space-y-4">
             {images.map((item) => (
-              <Link href={`/sneakers/${item.route}`}>
-              <div key={item.alt} className=" flex flex-col items-center justify-center px-4">
+              <Link key={item.alt} href={`/sneakers/${item.route}`}>
+              <div className=" flex flex-col items-center justify-center px-4">
                 <Image 
                   src={item.src} 
                   alt={item.alt} 
