c045eb7cf47a2de32bfe393eaf65568e4cc8c404
Sriabhinay Kusuma <sriabhinayk@gmail.com>
2024-02-13 12:28:25 -0600

    commit

--- src/app/components/GetHomePageImages.js
+++ src/app/components/GetHomePageImages.js
@@ -1,31 +1,27 @@
 import React from 'react'
 import Carousel_App from '@/app/client_components/CarouselApp' //client code
-import image1 from '../../../public/HomePageImages/image1.jpg'
-import image2 from '../../../public/HomePageImages/image2.jpg'
-import image3 from '../../../public/HomePageImages/image3.jpg'
-import image4 from '../../../public/HomePageImages/image4.jpg'
-import image5 from '../../../public/HomePageImages/image5.jpg'
+
 async function GetHomePageImages() {
  
  const allImages = [
     {
-      src:image5,
+      src:'/HomePageImages/image5.jpg',
       alt:"All Air Jordan 1",
       route:'AJ1',
     },{
-      src:image3,
+      src:'/HomePageImages/image3.jpg',
       alt:"All Air Jordan 3",
       route:'AJ3',
     },{
-      src:image1,
+      src:'/HomePageImages/image1.jpg',
       alt:"All Yeezy 350",
       route:'Y350',
     },{
-      src:image4,
+      src:'/HomePageImages/image4.jpg',
       alt:"All Yeezy 700",
       route:'Y700',
     },{
-      src:image2,
+      src:'/HomePageImages/image2.jpg',
       alt:"All Air Jordan 7",
       route:'AJ7',
     },
