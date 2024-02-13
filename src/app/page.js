07dc755c35d5fe6a65466e2158878791fbbc4764
Sriabhinay Kusuma <sriabhinayk@gmail.com>
2024-02-13 14:51:54 -0600

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -29,30 +29,18 @@ export default function Home() {
 
 ]
  
-  return (
+  return (  
     <div className="flex justify-center items-center p-5 mt-10">
-      
+     
       <div className="flex justify-center border border-gray-300 w-full max-w-[1000px] h-[500px] md:w-[1000px] md:h-[400px] lg:h-[420px] md:block sm:hidden overflow-y-auto" >
         <div className="flex justify-center flex-col items-center w-full">
             
-            {/* <GetHomePageImages/> */}
+            <GetHomePageImages/>
          
         </div>
       </div>
  
-      <div>
-        {
-        allImages.map((item)=>{
-         return <div key={item.alt}>
-        <p>
-            {item.alt}
-        </p>
-        <Image src={item.src} width={300} height={300}>
-        </Image>
-         </div>
-        })
-      }
-      </div>
+     
 
     </div>
   );
