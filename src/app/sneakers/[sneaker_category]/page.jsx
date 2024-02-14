59803e2326ea39b072ae9be4a29c6526add54315
Sriabhinay Kusuma <sriabhinayk@gmail.com>
2024-02-13 23:53:25 -0600

    commit

--- src/app/sneakers/[sneaker_category]/page.jsx
+++ src/app/sneakers/[sneaker_category]/page.jsx
@@ -36,7 +36,7 @@ async function Page({ params }) {
               <Link href={`sneakers/${result.data.id}`} key={result.data.id} className="border border-gray-300 p-2 m-2 text-center w-72 block hover:bg-gray-200 hover:text-black cursor-pointer">
                 <h3 className="font-semibold">{result.data.slug}</h3>
                 <div className="image-container h-70">
-                  <Image src={result.data.image_url} alt={result.data.slug} layout="responsive" width={300} height={500} />
+                  <Image src={result.data.image_url} alt={result.data.slug} layout="responsive" width={300} height={500} priority/>
                 </div>
                 <p>Retail Price: ${convertToUSD(result.data.retail_price_cents)}</p>
               </Link>
