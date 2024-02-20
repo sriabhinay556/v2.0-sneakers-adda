264cac4b0be5525f8b1fdf45a10bae6cb9c12247
Sriabhinay Kusuma <sriabhinayk@gmail.com>
2024-02-20 13:52:58 -0600

    commit

--- src/app/api/[id]/route.js
+++ src/app/api/[id]/route.js
@@ -6,13 +6,15 @@ export async function GET(request,{params}){
    // console.log('hello')
     let data
     try {
+        
         const sneakerId = params.id // params from above gives the routes
         const res = await fetch(`https://www.goat.com/web-api/v1/product_variants/buy_bar_data?productTemplateId=${sneakerId}&countryCode=US`
         , {   method: 'GET', // Specify the method
-        mode: 'cors', // This should be 'cors' for CORS requests. Use 'no-cors' only if you understand the limitations.
+        mode: 'no-cors', // This should be 'cors' for CORS requests. Use 'no-cors' only if you understand the limitations.
         cache: 'no-store', // Tells the browser not to cache the response. })
         })
         data = await res.json()
+        //data.headers = 'Access-Control-Allow-Origin', '*'
         // const headers = new Headers(request.headers);
         // headers.set('Access-Control-Allow-Origin', '*');
        //console.log("headers: " , headers())
--- src/app/price/[id]/page.jsx
+++ src/app/price/[id]/page.jsx
@@ -5,7 +5,7 @@ async function page({params}) {
     const sneakerId = params.id
     const res = await fetch(`https://www.goat.com/web-api/v1/product_variants/buy_bar_data?productTemplateId=${sneakerId}&countryCode=US`, {
         method: 'GET', // Specify the method
-        mode: 'cors', // This should be 'cors' for CORS requests. Use 'no-cors' only if you understand the limitations.
+        mode: 'no-cors', // This should be 'cors' for CORS requests. Use 'no-cors' only if you understand the limitations.
         cache: 'no-store', // Tells the browser not to cache the response.
       });
       
