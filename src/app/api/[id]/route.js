0889711ec59e9d0a1ab346bdc7e895bca9f41fb1
Sriabhinay Kusuma <sriabhinayk@gmail.com>
2024-02-20 15:11:28 -0600

    commit

--- src/app/api/[id]/route.js
+++ src/app/api/[id]/route.js
@@ -1,26 +1,24 @@
-
 import { NextResponse } from "next/server";
-import { NextApiResponse } from 'next';
-import { headers } from "next/headers";
-export async function GET(request,{params}){
-    let data
+
+export async function GET(request, { params }) {
+    let data, errorMessage;
+
     try {
-        
-        const sneakerId = params.id // params from above gives the routes
-        const res = await fetch(`https://www.goat.com/web-api/v1/product_variants/buy_bar_data?productTemplateId=${sneakerId}&countryCode=US`
-        ,{cache: "no-store"})
-        data = await res.json()
-        if(data.ok){
-            console.log(data)
+        const sneakerId = params.id; // params from above gives the routes
+        const res = await fetch(`https://www.goat.com/web-api/v1/product_variants/buy_bar_data?productTemplateId=${sneakerId}&countryCode=US`, { cache: "no-store" });
+
+        // Check if the response was ok (status in the range 200-299)
+        if (!res.ok) {
+            // Throw an error with response status to be caught in the catch block
+            throw new Error(`API request failed with status: ${res.status}`);
         }
-        else{
-            throw new Error('didnt fetch at route.js')
-        }
+
+        data = await res.json();
+    } catch (error) {
+        console.error(error);
+        errorMessage = error.message; // Store error message to include in the response
     }
-    catch (error) {
-        console.log(error)
-    }
-    // Return the response with CORS headers and JSON data
-    return new NextResponse(JSON.stringify({ prices: data }));
-    // return NextResponse.json({msg:'hello'})
-} 
+
+    // Include the error message in the response if there was an error
+    return new NextResponse(JSON.stringify({ prices: data, dummymessage: "data from route.js", error: errorMessage }));
+}
